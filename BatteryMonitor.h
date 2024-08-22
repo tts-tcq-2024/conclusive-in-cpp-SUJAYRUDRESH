2e3fec787648883cefc467de2cb5241ee6aebe98
SUJAYRUDRESH <60745093+SUJAYRUDRESH@users.noreply.github.com>
2024-08-22 23:56:33 +0530

    Create BatteryMonitor.h

---
+++ BatteryMonitor.h
@@ -0,0 +1,36 @@
+#include "IBreachClassifier.h"
+#include "IAlertHandler.h"
+#include <unordered_map>
+
+class BatteryMonitor {
+public:
+    BatteryMonitor(IBreachClassifier& classifier, IAlertHandler& controllerHandler, IAlertHandler& emailHandler)
+        : classifier(classifier), controllerHandler(controllerHandler), emailHandler(emailHandler) {}
+
+    void checkAndAlert(double temperature, bool isController) {
+        BreachType breachType = classifier.classify(temperature);
+        if (isController) {
+            handleControllerAlert(controllerHandler, breachType);
+        } else {
+            handleEmailAlert(emailHandler, breachType);
+        }
+    }
+
+private:
+    void handleControllerAlert(IAlertHandler& alertHandler, BreachType breachType) {
+        if (auto* controllerHandler = dynamic_cast<ControllerAlertHandler*>(&alertHandler)) {
+            controllerHandler->sendAlert(breachType);
+        }
+    }
+
+    void handleEmailAlert(IAlertHandler& alertHandler, BreachType breachType) {
+        auto messageLookup = breachTypeMessages.find(breachType);
+        if (messageLookup != breachTypeMessages.end() && !messageLookup->second.empty()) {
+            alertHandler.sendAlert(messageLookup->second);
+        }
+    }
+
+    IBreachClassifier& classifier;
+    IAlertHandler& controllerHandler;
+    IAlertHandler& emailHandler;
+};
