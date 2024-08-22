6ebbb8d7feb0d46a022851f0921cc79e5f36cd59
SUJAYRUDRESH <60745093+SUJAYRUDRESH@users.noreply.github.com>
2024-08-23 00:20:37 +0530

    Update IAlertHandler.h

--- IAlertHandler.h
+++ IAlertHandler.h
@@ -1,3 +1,6 @@
+#ifndef I_ALERT_HANDLER_H
+#define I_ALERT_HANDLER_H
+
 #include <string>
 #include "IBreachClassifier.h"
 
@@ -7,3 +10,5 @@ public:
     virtual void sendAlert(BreachType breachType) = 0;
     virtual void sendAlert(const std::string& message) = 0;
 };
+
+#endif // I_ALERT_HANDLER_H
