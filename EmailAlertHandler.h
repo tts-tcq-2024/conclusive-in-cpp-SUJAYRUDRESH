e0a3538d9c269be8cf2c33750d4e2aa7e382fda4
SUJAYRUDRESH <60745093+SUJAYRUDRESH@users.noreply.github.com>
2024-08-22 23:56:00 +0530

    Create EmailAlertHandler.h

---
+++ EmailAlertHandler.h
@@ -0,0 +1,15 @@
+#include "IAlertHandler.h"
+#include <iostream>
+
+class EmailAlertHandler : public IAlertHandler {
+public:
+    void sendAlert(BreachType breachType) override {
+        // Optionally handle BreachType if needed or leave unimplemented
+    }
+
+    void sendAlert(const std::string& message) override {
+        const char* recipient = "a.b@c.com";
+        std::cout << "To: " << recipient << std::endl;
+        std::cout << message << std::endl;
+    }
+};
